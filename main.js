c8726285abf847f9a00417a76cad4a7e0de269dd
Ubuntu <ubuntu@ip-172-31-7-92.ap-northeast-2.compute.internal>
2021-09-29 08:57:37 +0000

    feat: temp backup

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('Hello world1')
 });
 
-app.listen(3000, function(){
+app.listen(80, function(){
     console.log('Example app listening on port 3000!')
 });
