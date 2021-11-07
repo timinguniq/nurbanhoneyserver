47e02d8ca2e9cf5d469151175266be2779898ca8
timinguniq <timing.uniq@gmail.com>
2021-11-07 17:36:59 +0900

    feat: backup

--- utils/createrank.js
+++ utils/createrank.js
@@ -1,18 +1,23 @@
-const rankDao = require('../dbdao/rankdao');
 const nurbanBoardDao = require('../dbdao/nurbanboarddao');
+const rankDao = require('../dbdao/rankdao');
 
 // 한달에 한번 rank 데이터를 만드는 메소드
 module.exports = async () => {    
+    let contentObjectList = [];
     try{
         let resultList = await nurbanBoardDao.readForRank();
 
-        let contentObjectList = [];
-
         for(var i = 0 ; i < resultList.length ; i++){
             contentObjectList.push(resultList[i].dataValues);
-        }
-        
+        }        
+        console.log("contentObjectList : ", contentObjectList);
+    }catch(err){
+        console.log("nurbanBoardDao readForRank error", err);
+    }
+
+    try{
         let deleteResult = await rankDao.destoryAll();
+        console.log("delete Result : ", deleteResult);
         if(deleteResult === 1){
             for(var i = 0 ; i < contentObjectList.length ; i++){
                 let result = await rankDao.create(contentObjectList[i].sumLossCut, 
@@ -20,7 +25,8 @@ module.exports = async () => {
             }
         }        
     }catch(err){
-
+        console.log("rankDao destroyAll error")
     }
+
     console.log("1분 마다 실행");
 }
