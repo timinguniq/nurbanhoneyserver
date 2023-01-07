53aae71c40a0f279f7eb1f74c8d4c7f6b381d02f
timinguniq <timing.uniq@gmail.com>
2023-01-07 15:32:19 +0900

    feat: token change

--- router/tokenmidrouter.js
+++ router/tokenmidrouter.js
@@ -7,7 +7,10 @@ var isValidToken = require('../utils/isvalidtoken.js');
 // 토큰 테스트
 router.use((req, res, next) => {
     // 나중에 테스트
-    let token = req.headers.token;
+    //let token = req.headers.token;
+    let auth = req.headers.authorization;
+    let token = auth.replace('Bearer ', '');
+    console.log('token : ', token);
     if(isValidToken(token)){
         // 토큰이 유효하다
         next();
