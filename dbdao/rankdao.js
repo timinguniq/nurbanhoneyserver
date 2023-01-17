4852f9c67063187c451157c3a2c401df68245d22
timinguniq <timing.uniq@gmail.com>
2023-01-17 17:35:07 +0900

    modify: rankdao destoryAll

--- dbdao/rankdao.js
+++ dbdao/rankdao.js
@@ -37,8 +37,5 @@ exports.readPopup = function read(offset = 0, limit = 3){
 // 모든 데이터 삭제
 exports.destoryAll = function destory(){
     // 전체 데이터 삭제
-    return Rank.destroy({
-        where: {},
-        truncate: true
-    })
+    return Rank.destoryAll()
 } 
