6e612ae2eaf85afa9938d1c76ee9ecd63051f6d1
timinguniq <timing.uniq@gmail.com>
2023-01-17 17:45:03 +0900

    modify: rankdao destoryAll

--- utils/createrank.js
+++ utils/createrank.js
@@ -18,7 +18,7 @@ module.exports = async () => {
     try{
         let rankResult = await rankDao.read();
         console.log('rankResult : ', rankResult);
-        if(rankResult === []){
+        if(rankResult === '[]'){
             console.log('rankResult empty');
             for(var i = 0 ; i < contentObjectList.length ; i++){
                 let result = await rankDao.create(contentObjectList[i].sumLossCut, 
