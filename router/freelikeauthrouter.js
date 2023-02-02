cb510ed221eaac49194c1e1b7482706c46036f0d
timinguniq <timing.uniq@gmail.com>
2023-02-02 17:20:02 +0900

    modify: free delete like

--- router/freelikeauthrouter.js
+++ router/freelikeauthrouter.js
@@ -105,7 +105,7 @@ router.delete('/', async (req, res) => {
         return res.end();
     }
 
-    let token = req.headers.token;
+    let token = req.headers.authorization?.replace('Bearer ', '');
 
     // 토큰에서 키 값 추출
     let key = extractKey(token);
