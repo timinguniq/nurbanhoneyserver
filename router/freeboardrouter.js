80bd1c5009e098c69ad4d8dfb8b03aebe2e62712
timinguniq <timing.uniq@gmail.com>
2023-04-12 18:59:19 +0900

    feat: bakcup

--- router/freeboardrouter.js
+++ router/freeboardrouter.js
@@ -138,10 +138,10 @@ router.get('/', async (req, res) => {
             result[i].dataValues.user.dataValues.insignia = await getInsigniaShown(result[i].dataValues.user.dataValues.userId);
 
             // string으로 안 가고 array로 가게 수정하는 코드
-            result[i].dataValues.user.dataValues.insignia = JSON.parse(result[i].dataValues.user.dataValues.insignia);
-            if(result[i].dataValues.user.dataValues.insignia === ""){
-                result[i].dataValues.user.dataValues.insignia = [];
-            }
+            //result[i].dataValues.user.dataValues.insignia = JSON.parse(result[i].dataValues.user.dataValues.insignia);
+            //if(result[i].dataValues.user.dataValues.insignia === ""){
+            //    result[i].dataValues.user.dataValues.insignia = [];
+            //}
             //
             contentObjectList.push(result[i].dataValues);
         }
