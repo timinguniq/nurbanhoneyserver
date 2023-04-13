7f150a575ed455ed811eeb870fc3e9998d715577
timinguniq <timing.uniq@gmail.com>
2023-04-13 17:26:02 +0900

    feat: bakcup

--- router/nurbanboardauthrouter.js
+++ router/nurbanboardauthrouter.js
@@ -214,7 +214,7 @@ router.post('/upload/image', async (req, res) => {
         return res.end();
     }
 
-    let nurbanArticle = nurbanBoardDao.readForUuid(uuid);
+    let nurbanArticle = await nurbanBoardDao.readForUuid(uuid);
     console.log('nurbanArticle : ', nurbanArticle);
 
     let imageFileNameSize = JSON.stringify(imageFiles[0].originalname).split('\\').length;
