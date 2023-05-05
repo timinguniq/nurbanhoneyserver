7bc6ea5598e4cca4f029a53d876005323eb6c2c6
timinguniq <timing.uniq@gmail.com>
2023-05-05 18:30:00 +0900

    fix: createnurbanmyrating

--- utils/createnurbanmyrating.js
+++ utils/createnurbanmyrating.js
@@ -1,12 +1,16 @@
 const nurbanDislikeDao = require('../dbdao/nurbandislikedao');
 const nurbanLikeDao = require('../dbdao/nurbanlikedao');
 
+const totalDislikeDao = require('../dbdao/totaldislikedao');
+const totalLikeDao = require('../dbdao/totallikedao');
+
 // 너반꿀 게시판 myrating을 만드는 통신
 module.exports = async (articleId, userId) => {
     let myRating = null;
     // 좋아요 데이터 받아오는 코드
     try{
-        let like = await nurbanLikeDao.read(articleId, userId);
+        //let like = await nurbanLikeDao.read(articleId, userId);
+        let like = await totalLikeDao.read(articleId, userId);
         console.log("like result", like);
         if(like !== null){
             myRating = 'like'; 
@@ -16,7 +20,8 @@ module.exports = async (articleId, userId) => {
     }
     // 싫어요 데이터 받아오는 코드
     try{
-        let dislike = await nurbanDislikeDao.read(articleId, userId);
+        //let dislike = await nurbanDislikeDao.read(articleId, userId);
+        let dislike = await totalDislikeDao.read(articleId, userId);
         console.log("dislike result", dislike);
         if(dislike !== null){
             myRating = 'dislike';
