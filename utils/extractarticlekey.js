c991b18d9066e763a41319231406655f35c9b4c4
timinguniq <timing.uniq@gmail.com>
2023-05-09 17:31:18 +0900

    feat: nurbanboard, freeboard dao, model(5)

--- utils/extractarticlekey.js
+++ utils/extractarticlekey.js
@@ -1,7 +1,5 @@
 const totalBoardDao = require('../dbdao/totalboarddao');
 
-const totalBoardDao = require('../dbdao/totalboarddao');
-
 // articleId를 가지고 그 글을 작성 사용자의 key값을 추출한다.
 // input값이 articleId값이고 key 값을 추츨한다.
 module.exports = async (articleId) => {
