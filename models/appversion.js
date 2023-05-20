ff906b488cf7c1e83c5cd0033c80aeccde0ffc36
timinguniq <timing.uniq@gmail.com>
2023-05-20 22:58:02 +0900

    feat: create rank timek

--- models/appversion.js
+++ models/appversion.js
@@ -26,14 +26,5 @@ module.exports = (sequelize, DataTypes) => {
       timestamps: true, // createAt & updateAt 활성화
       paranoid: true, // timestamps 가 활성화 되어야 사용 가능 > deleteAt 옵션 on
     });
-    /*
-    Users.associate = models => {
-      /**
-       * Users안에 있는 "id값"을 "user_id라는 컬럼 이름"으로 UserInfo모델에 새로운 컬럼으로 추가한다.
-       */
-      /*
-      Users.hasOne(models.UserInfo, {foreignKey: "user_id", sourceKey: 'id'});
-    };
-    */
     return appversion;
   };
