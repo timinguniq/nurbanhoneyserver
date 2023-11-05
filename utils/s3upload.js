4ce27315776b72f0d39f9805042f4cad2581fa2e
timinguniq <timing.uniq@gmail.com>
2023-11-05 16:38:46 +0900

    feat: backup

--- utils/s3upload.js
+++ utils/s3upload.js
@@ -23,12 +23,10 @@ module.exports = async (bucketName, folderName, imageFileName, bodyBuffer, callb
         if(err !== null){
             // 에러가 있음
             let resultObject = createJson.error(err);
-            console.log('s3 upload err : ', err);
             return callback(resultObject)
         }else{
             // 에러가 없음
             let resultObject = createJson.result(data.Location);
-            console.log('s3 upload resultObejct : ', resultObject);
             return callback(resultObject)
         }
     });
