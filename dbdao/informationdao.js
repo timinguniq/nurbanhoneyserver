b5ffa3870f2b466f350bfe7b65b89abf1e077d3a
timinguniq <timing.uniq@gmail.com>
2024-05-12 14:18:28 +0900

    feat: information router init(3)

--- dbdao/informationdao.js
+++ dbdao/informationdao.js
@@ -1,11 +1,11 @@
 const Information = require('../models').information;
 
 // 글을 id로 갯수 가져오기
-exports.readForFlag = function read(flag){
+exports.readForFlag = function read(type){
     return Information.findOne({
         attributes: ['content'],
         where: {
-            flag: flag,
+            type: type,
         }
     })
 }
