88fa73c49be00737a2ce14dbdc63fa7cc35dbcb5
timinguniq <timing.uniq@gmail.com>
2024-05-12 13:29:57 +0900

    feat: informationdao

---
+++ dbdao/informationdao.js
@@ -0,0 +1,11 @@
+const Information = require('../models').information;
+
+// 글을 id로 갯수 가져오기
+exports.readForFlag = function read(flag){
+    return Information.findOne({
+        attributes: ['content'],
+        where: {
+            id: id 
+        }
+    })
+}
