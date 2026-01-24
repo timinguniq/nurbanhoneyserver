7956813f8496daa77fed7dfeacb6310b138385cd
jehee <timing.uniq@gmail.com>
2026-01-24 16:12:52 +0900

    feat: local data save and delete

--- router/freeboardauthrouter.js
+++ router/freeboardauthrouter.js
@@ -4,8 +4,8 @@ const userDao = require('../dbdao/userdao');
 var createJson = require('../utils/createjson');
 var extractKey = require('../utils/extractkey');
 var extractUserId = require('../utils/extractuserid');
-var s3upload = require('../utils/s3upload');
-var s3delete = require('../utils/s3delete');
+var localUpload = require('../utils/localupload');
+var localDelete = require('../utils/localdelete');
 let inputErrorHandler = require('../utils/inputerrorhandler');
 let awsObj = require('../config/aws');
 let constObj = require('../config/const');
@@ -30,8 +30,8 @@ router.post('/', async (req, res) => {
 
     // 필수 input 값이 null이거나 undefined면 에러
     let inputArray = [uuid, title, content];
-    if(await inputErrorHandler(inputArray)){
-        resultObject = createJson.error("input is null");   
+    if (await inputErrorHandler(inputArray)) {
+        resultObject = createJson.error("input is null");
         res.status(400).json(resultObject);
         return res.end();
     }
@@ -42,10 +42,10 @@ router.post('/', async (req, res) => {
     let key = extractKey(token);
 
     // key 값으로 User 테이블의 id 값 받아오기
-    try{
+    try {
         let result = await userDao.read(key);
         userId = result.id;
-    }catch(err){
+    } catch (err) {
         console.log(`userDao err : ${err}`);
         resultObject = createJson.error(err);
         res.status(500).json(resultObject);
@@ -53,20 +53,20 @@ router.post('/', async (req, res) => {
     }
 
     // 너반꿀 게시판 글 작성
-    try{
+    try {
         //let result = await freeBoardDao.create(uuid, thumbnail, title, content, userId);
         /// create(uuid, board, thumbnail, title, lossCut, content, userId)
         let result = await totalBoardDao.create(uuid, constObj.free, thumbnail, title, lossCut, content, userId);
         console.log(`create : ${result}`);
 
         // 포인트를 올리는 메소드
-        if(!raisePoint(key, constObj.writeArticlePoint)){
+        if (!raisePoint(key, constObj.writeArticlePoint)) {
             console.log("raisePoint error");
         }
 
         resultObject = createJson.result("freeboard_posted");
         res.status(201).json(resultObject);
-    }catch(err){
+    } catch (err) {
         console.log(`create freeboardDao err : ${err}`);
         resultObject = createJson.error(err);
         res.status(400).json(resultObject);
@@ -80,32 +80,32 @@ router.patch('/', async (req, res) => {
     let thumbnail = req.body.thumbnail;
     let title = req.body.title;
     let content = req.body.content;
-    
+
     let contentObject = new Object();
     let resultObject = new Object();
 
     // 필수 input 값이 null이거나 undefined면 에러
     let inputArray = [id, thumbnail, title, content];
-    if(await inputErrorHandler(inputArray)){
+    if (await inputErrorHandler(inputArray)) {
         resultObject = createJson.error("input is null");
         res.status(400).json(resultObject);
         return res.end();
     }
 
-    try{
+    try {
         //let result = await freeBoardDao.updateContent(id, thumbnail, title, content);
         let result = await totalBoardDao.updateContent(id, thumbnail, title, null, content);
-        
+
         // result 1이면 성공 0이면 실패
         console.log(`patch result : ${result}`)
-        if(result[0] === 1){
+        if (result[0] === 1) {
             resultObject = createJson.result("freeboard_updated");
             res.status(200).json(resultObject);
-        }else{
+        } else {
             resultObject = createJson.result("freeboard_updated_fail");
             res.status(700).json(resultObject);
         }
-    }catch(err){
+    } catch (err) {
         console.log(`patch err : ${err}`)
         resultObject = createJson.error(err);
         res.status(500).json(resultObject);
@@ -125,13 +125,13 @@ router.delete('/', async (req, res) => {
 
     // 필수 input 값이 null이거나 undefined면 에러
     let inputArray = [id, uuid];
-    if(await inputErrorHandler(inputArray)){
+    if (await inputErrorHandler(inputArray)) {
         resultObject = createJson.error("input is null");
         res.status(400).json(resultObject);
         return res.end();
     }
 
-    if(token !== null && token !== undefined){      
+    if (token !== null && token !== undefined) {
         // 토큰에서 키 값 추출
         key = extractKey(token);
 
@@ -140,12 +140,12 @@ router.delete('/', async (req, res) => {
     }
 
     let deleteResult = null;
-    try{
+    try {
         //let readResult = await freeBoardDao.readForId(id);
         let readResult = await totalBoardDao.readForId(id);
         console.log("readResult userId : ", readResult);
 
-        if(readResult === null){
+        if (readResult === null) {
             resultObject = createJson.error("article is not exist");
             res.status(404).json(resultObject);
             return res.end();
@@ -154,22 +154,22 @@ router.delete('/', async (req, res) => {
         let articleUserId = readResult.userId;
         console.log("readResult userId 2 : ", articleUserId);
 
-        if(userId !== articleUserId){
+        if (userId !== articleUserId) {
             resultObject = createJson.error("access impossible");
             res.status(401).json(resultObject);
             return res.end();
-        }else{
+        } else {
             //deleteResult = await freeBoardDao.destroy(id);
             deleteResult = await totalBoardDao.destroy(id);
         }
-                
+
         // deleteResult 1이면 성공 0이면 실패
         console.log(`delete result : ${deleteResult}`)
-        
-        if(deleteResult === 1){
+
+        if (deleteResult === 1) {
             resultObject = createJson.result("freeboard_deleted");
             res.status(200).json(resultObject);
-        }else{
+        } else {
             resultObject = createJson.result("freeboard_deleted_fail");
             res.status(700).json(resultObject);
             return res.end();
@@ -180,23 +180,23 @@ router.delete('/', async (req, res) => {
         // 좋아요 싫어요 정산 포인트 
         let diffPoint = diffLikeDislikeCount * constObj.likePoint;
         // 좋아요 싫어요 포인트 정산
-        if(!raisePoint(key, diffPoint)){
+        if (!raisePoint(key, diffPoint)) {
             console.log("raisePoint error");
         }
         // 포인트를 내리는 메소드
-        if(!dropPoint(key, constObj.writeArticlePoint)){
+        if (!dropPoint(key, constObj.writeArticlePoint)) {
             console.log("dropPoint error");
         }
-    }catch(err){
+    } catch (err) {
         console.log(`delete err : ${err}`)
         resultObject = createJson.error(err);
         res.status(500).json(resultObject);
         return res.end();
     }
 
-    if(deleteResult === 1){
-        // s3에 글 이미지 삭제하기
-        s3delete(awsObj.s3freeboardname, uuid);
+    if (deleteResult === 1) {
+        // 로컬에 글 이미지 삭제하기
+        localDelete(awsObj.s3freeboardname, uuid);
     }
 });
 
@@ -207,27 +207,27 @@ router.post('/upload/image', async (req, res) => {
 
     let contentObject = new Object();
     let resultObject = new Object();
-    
+
     // 필수 input 값이 null이거나 undefined면 에러
     let inputArray = [imageFiles, uuid];
-    if(await inputErrorHandler(inputArray)){
+    if (await inputErrorHandler(inputArray)) {
         resultObject = createJson.error("input is null");
         res.status(400).json(resultObject);
         return res.end();
     }
 
     let imageFileNameSize = JSON.stringify(imageFiles[0].originalname).split('\\').length;
-    let imageFileName = JSON.stringify(imageFiles[0].originalname).split('\\')[imageFileNameSize-1];
-    
-    let bufferObj = JSON.parse(JSON.stringify(imageFiles[0].buffer));
-    let bodyBuffer = new Buffer.from(bufferObj.data);  
+    let imageFileName = JSON.stringify(imageFiles[0].originalname).split('\\')[imageFileNameSize - 1];
 
-    // s3에 파일 업로드 하는 메소드
-    s3upload(awsObj.s3freeboardname, uuid, imageFileName, bodyBuffer, (resultObject) => {
-        if(resultObject.result !== null && resultObject.result !== undefined){
+    let bufferObj = JSON.parse(JSON.stringify(imageFiles[0].buffer));
+    let bodyBuffer = new Buffer.from(bufferObj.data);
+
+    // 로컬 파일 업로드 하는 메소드
+    localUpload(awsObj.s3freeboardname, uuid, imageFileName, bodyBuffer, (resultObject) => {
+        if (resultObject.result !== null && resultObject.result !== undefined) {
             res.status(200).json(resultObject);
-        }else{
-            res.status(500).json(resultObject);    
+        } else {
+            res.status(500).json(resultObject);
         }
     });
 });
@@ -235,28 +235,28 @@ router.post('/upload/image', async (req, res) => {
 // 글 관련 이미지 삭제하는 통신
 router.delete('/upload/image', async (req, res) => {
     let uuid = req.body.uuid;
-    
+
     let contentObject = new Object();
     let resultObject = new Object();
 
     // 필수 input 값이 null이거나 undefined면 에러
     let inputArray = [uuid];
-    if(await inputErrorHandler(inputArray)){
+    if (await inputErrorHandler(inputArray)) {
         resultObject = createJson.error("input is null");
         res.status(400).json(resultObject);
         return res.end();
     }
 
-    try{
-        // s3에 글 이미지 삭제하기
-        let result = await s3delete(awsObj.s3freeboardname, uuid);
+    try {
+        // 로컬 글 이미지 삭제하기
+        let result = await localDelete(awsObj.s3freeboardname, uuid);
 
         resultObject = createJson.result("freeboard_image_deleted");
         res.status(200).json(resultObject);
-    }catch(err){
+    } catch (err) {
         resultObject = createJson.error("error");
         res.status(500).json(resultObject);
-    }    
+    }
 });
 
 module.exports = router;
---
+++ utils/localupload.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const createJson = require('../utils/createjson');
+
+// 로컬 스토리지에 파일 업로드 하는 메소드
+// bucketName은 S3 호환성을 위해 남겨두지만 실제로는 무시하거나 최상위 폴더명으로 사용할 수 있음
+module.exports = async (bucketName, folderName, imageFileName, bodyBuffer, callback) => {
+    // public/uploads 폴더 기반
+    const uploadDir = path.join(__dirname, '../public/uploads', folderName);
+
+    // 디렉토리가 없으면 생성
+    if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+    }
+
+    const filePath = path.join(uploadDir, imageFileName);
+
+    // 파일 쓰기
+    fs.writeFile(filePath, bodyBuffer, (err) => {
+        if (err) {
+            console.error('Local upload error:', err);
+            let resultObject = createJson.error(err);
+            return callback(resultObject);
+        } else {
+            // 웹 접근 가능한 상대 경로 반환 (예: /uploads/uuid/filename.png)
+            // bucketName이 URL에 포함되어야 한다면 조정 필요. 현재는 folderName/imageFileName 구조
+            const publicUrl = `/uploads/${folderName}/${imageFileName}`;
+            let resultObject = createJson.result(publicUrl);
+            return callback(resultObject);
+        }
+    });
+}
